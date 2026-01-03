68c14cd89ce366b1ac9cec58469409276bd417ee
Milan Hauth <milahu@gmail.com>
2026-01-03 11:02:34 +0100

    add variables: owner repo

--- scripts/git-remote-add.sh
+++ scripts/git-remote-add.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+owner=milahu
+repo=darknet-git-hosting-services
+
 function git_remote_add_onion() {
   local remote="$1"
   local url="$2"
@@ -8,6 +11,6 @@ function git_remote_add_onion() {
   git config --add remote."$remote".proxy socks5h://127.0.0.1:9050
 }
 
-git_remote_add_onion darktea.onion http://it7otdanqu7ktntxzm427cba6i53w6wlanlh23v5i3siqmos47pzhvyd.onion/milahu/darknet-git-hosting-services
+git_remote_add_onion darktea.onion http://it7otdanqu7ktntxzm427cba6i53w6wlanlh23v5i3siqmos47pzhvyd.onion/$owner/$repo
 
-git_remote_add_onion righttoprivacy.onion http://gg6zxtreajiijztyy5g6bt5o6l3qu32nrg7eulyemlhxwwl6enk6ghad.onion/milahu/darknet-git-hosting-services
+git_remote_add_onion righttoprivacy.onion http://gg6zxtreajiijztyy5g6bt5o6l3qu32nrg7eulyemlhxwwl6enk6ghad.onion/$owner/$repo
